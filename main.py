afdaea5664caa27b2f50b3c84bed0575759ae341
juan-castillo-ltv <juan.castillo@ltv.fund>
2024-05-15 15:26:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -58,45 +58,5 @@ def track_event():
     
     return jsonify({"success": "Event tracked successfully"}), 200
 
-@app.route('/pageview', methods=['POST'])
-def track_pageview():
-    pageview_data = request.get_json()
-    if not pageview_data:
-        return jsonify({"error": "Invalid data"}), 400
-
-    conn = connection_pool.getconn()
-    if conn is None:
-        logging.error("Failed to connect to the database")
-        return jsonify({"error": "Database connection error"}), 500
-
-    cur = conn.cursor()
-    try:
-        cur.execute("""
-            INSERT INTO app_pageviews_tracking (event_type, event_name, user_id, utm_source, utm_medium, utm_campaign, utm_content, time_of_event, event_url, app)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-        """, (
-            pageview_data.get('eventType'),
-            pageview_data.get('eventName'),
-            pageview_data.get('userId'),
-            pageview_data.get('utmSource'),
-            pageview_data.get('utmMedium'),
-            pageview_data.get('utmCampaign'),
-            pageview_data.get('utmContent'),
-            pageview_data.get('timeOfEvent'),
-            pageview_data.get('eventUrl'),
-            pageview_data.get('appName')
-        ))
-        conn.commit()
-        logging.info("Pageview tracked successfully")
-    except Exception as e:
-        logging.error(f"Failed to insert pageview data: {e}")
-        conn.rollback()
-        return jsonify({"error": "Failed to insert pageview data"}), 500
-    finally:
-        cur.close()
-        connection_pool.putconn(conn)
-
-    return jsonify({"success": "Pageview tracked successfully"}), 200
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
